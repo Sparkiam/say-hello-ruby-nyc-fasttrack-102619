dbe4f4310455f2ecb264e070c0473ddfff6c8cc8
Dell Wilson <warren.wilsonjr89@gmail.com>
2019-10-26 18:30:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,7 @@
 # Build your say_hello method here
 
-def say_hello
-  puts "Hello" + say_hello
-  intro("Gabriela!")
+def say_hello{name= "Ruby Programer"
+  puts "Hello #{name}!"
+end 
+
+say_hello("Gabriela")
